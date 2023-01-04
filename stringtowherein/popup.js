9dedfe97ffc940e895a55b3b3b1a2860814a40f4
Fairy <44420123+fairytx@users.noreply.github.com>
2023-01-04 15:06:24 +0800

    在( )前新增in

--- stringtowherein/popup.js
+++ stringtowherein/popup.js
@@ -1,6 +1,6 @@
 const str1 = document.getElementById('str1');
 str1.onchange = async function(){
-    str1.value='(\''+this.value.replace(/(\r\n|\r|\n)/g, '\\n').split('\\n').join('\',\'')+'\')';
+    str1.value='in (\''+this.value.replace(/(\r\n|\r|\n)/g, '\\n').split('\\n').join('\',\'')+'\')';
     str1.select();
     await navigator.clipboard.writeText(str1.value);
 }
